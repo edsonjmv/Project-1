561fba3f3dd1d95589a0bd8e92e28120ea1fc474
edsonjmv <edsonjmv@gmail.com>
2017-04-17 12:13:02 +0200

    monsters en dom

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ $(document).ready(function(){
   console.log("Linked!");
 
   var addMonster = setInterval(function(){
-    var initialX = Math.floor(Math.random() * 1000);
+    var initialX = Math.floor(Math.random() * 850);
     var initialY = Math.floor(Math.random() * 600);
     var monster = new Monster(initialX, initialY);
     monster.randomDirection();
     console.log(monster);
+    $("#board").append("<div class='monster' style=top:"+initialY+"px;left:"+initialX+"px;></div>");
   }, 5000);
 
   var addFruit = setInterval(function(){
-    var initialX = Math.floor(Math.random() * 1000);
+    var initialX = Math.floor(Math.random() * 850);
     var initialY = Math.floor(Math.random() * 600);
     var fruit = new Fruit(initialX, initialY);
     fruit.randomDirection();
