a5e9df311c68043874e4e075e77185bdb70a4cf7
edsonjmv <edsonjmv@gmail.com>
2017-04-18 11:56:05 +0200

    Scores

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
 $(document).ready(function(){
   var score = 0;
   var game = new Board();
+  var monsterQuantity;
 
   var addMonster = setInterval(function(){
     var randomID = Math.floor(Math.random() * 1000000);
@@ -10,15 +10,10 @@ $(document).ready(function(){
     var initialY = Math.floor(Math.random() * 500);
     var monster = new Monster(monsterID, initialX, initialY);
     monster.randomDirection();
-    game.addMonster();
+    game.addMonster(monster);
     console.log(monster);
     $("#board").append("<div id="+monsterID+" class='monster' style=top:"+initialY+"px;left:"+initialX+"px;></div>");
-    $(".monster").on("click", function() {
-      score += 1;
-      this.remove();
 
-      $("#points").html("<h1>"+score+"</h1>");
-    });
       var dir= monster.randomDirection();
       var moveX = 0;
       var moveY = 5;
@@ -27,20 +22,28 @@ $(document).ready(function(){
 
       var movingMonster = setInterval(function () {
         monster.move(moveX, moveY);
-        if (monster.y<=0) {
-          moveY=10;
-        } else if (monster.y>=540) {
-          moveY=-10;
+        if (monster.y <= 0) {
+          moveY = 10;
+        } else if (monster.y >= 540) {
+          moveY =- 10;
         }
-        if (monster.x<=0) {
-          moveX=10;
-        } else if (monster.x>=940) {
-          moveX=-10;
+        if (monster.x <= 0) {
+          moveX = 10;
+        } else if (monster.x >= 940) {
+          moveX =- 10;
         }
       }, 100);
   }, 5000);
 
+  $(document).on("click", ".monster", function() {
+    score += 1;
+    console.log(score);
+    this.remove();
+    $("#points").html("<h1>"+score+"</h1>");
+  });
 
+  // monsterQuantity = game.monsterArmy.length;
+  // console.log(game);
 
   var addFruit = setInterval(function(){
   var randomID = Math.floor(Math.random() * 1000000);
@@ -51,30 +54,35 @@ $(document).ready(function(){
     fruit.randomDirection();
     console.log(fruit);
     $("#board").append("<div id="+fruitID+"  class='fruit' style=top:"+initialY+"px;left:"+initialX+"px;></div>");
-    $(".fruit").on("click", function() {
-      score -= 1;
-      this.remove();
 
-    });
-      var dir= fruit.randomDirection();
+      var dirF= fruit.randomDirection();
       var moveX = 0;
       var moveY = 5;
-      if (dir === "left") moveX = -10;
+      if (dirF === "left") moveX = -10;
       else moveX = 10;
 
       var movingFruit = setInterval(function () {
         fruit.move(moveX, moveY);
-        if (fruit.y<=0) {
-          moveY=10;
-        } else if (fruit.y>=540) {
-          moveY=-10;
+        if (fruit.y <= 0) {
+          moveY = 10;
+        } else if (fruit.y >= 540) {
+          moveY =- 10;
         }
-        if (fruit.x<=0) {
-          moveX=10;
-        } else if (fruit.x>=940) {
-          moveX=-10;
+        if (fruit.x <= 0) {
+          moveX = 10;
+        } else if (fruit.x >= 940) {
+          moveX =- 10;
         }
       }, 100);
 }, 10000);
+
+$(document).on("click", ".fruit", function() {
+  score -= 1;
+  console.log(score);
+  this.remove();
+  $("#points").html("<h1>"+score+"</h1>");
+});
+
+// $("#quantM").html("<h1>"+monsterArmy.length+"</h1>");
 
 });
